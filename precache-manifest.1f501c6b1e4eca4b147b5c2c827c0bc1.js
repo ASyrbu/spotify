9ffd498fef0777cdbc10faa30c72a337887ff6ab
Alex Sterpu <alexander.sterpu@medikura.com>
2023-08-02 16:10:52 +0300

    Updates

---
+++ precache-manifest.1f501c6b1e4eca4b147b5c2c827c0bc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5aba61251d5e86836c84",
+    "url": "/spotify/static/css/main.9d3d6d80.chunk.css"
+  },
+  {
+    "revision": "5aba61251d5e86836c84",
+    "url": "/spotify/static/js/main.5aba6125.chunk.js"
+  },
+  {
+    "revision": "3e3c3eb55c09e01a1300",
+    "url": "/spotify/static/js/1.3e3c3eb5.chunk.js"
+  },
+  {
+    "revision": "20019c294ea6a7cce4c0",
+    "url": "/spotify/static/js/2.20019c29.chunk.js"
+  },
+  {
+    "revision": "5b50143acbb4d523a250",
+    "url": "/spotify/static/js/runtime~main.5b50143a.js"
+  },
+  {
+    "revision": "4c3d59d290887277faafc4f6812860bd",
+    "url": "/spotify/static/media/spotify-logo-transparent-free-png.4c3d59d2.webp"
+  },
+  {
+    "revision": "61345b758831e36b2db4a73ce3cbf185",
+    "url": "/spotify/index.html"
+  }
+];
